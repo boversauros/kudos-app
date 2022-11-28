f81ca4f67719c37dc408e60f2fdf6f479e6a7532
Oriol <bover73@gmail.com>
2022-11-28 13:26:12 +0100

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-700">
+      <h2 className="text-5xl font-extrabold text-blue-600">
+        TailwindCSS is working
+      </h2>
+    </div>
+  )
+}
