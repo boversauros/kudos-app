856b9d5c74fca39c5c16d810fdb51813d1364493
Oriol Bover Vila <oriolbovervila@Oriols-Mac-mini.local>
2022-12-28 18:31:54 +0100

    add the login functionality

--- app/components/user-panel.tsx
+++ app/components/user-panel.tsx
@@ -17,12 +17,14 @@ export function UserPanel({ users }: { users: User[] }) {
         ))}
       </div>
       <div className="bg-gray-300 p-6 text-center">
-        <button
-          type="submit"
-          className="rounded-xl bg-yellow-300 px-3 py-2 font-semibold text-blue-600 transition duration-300 ease-in-out hover:-translate-y-1 hover:bg-yellow-400"
-        >
-          Sign Out
-        </button>
+        <form action="/logout" method="post">
+          <button
+            type="submit"
+            className="rounded-xl bg-yellow-300 px-3 py-2 font-semibold text-blue-600 transition duration-300 ease-in-out hover:-translate-y-1 hover:bg-yellow-400"
+          >
+            Sign Out
+          </button>
+        </form>
       </div>
     </div>
   )
