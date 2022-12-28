f9190005b8f5eb20e957187453787174a9828889
Oriol Bover Vila <oriolbovervila@Oriols-Mac-mini.local>
2022-12-28 18:25:22 +0100

    add new user-circle component and load users from db

--- app/components/user-panel.tsx
+++ app/components/user-panel.tsx
@@ -1,11 +1,20 @@
-export function UserPanel() {
+import type { User } from '@prisma/client'
+import { UserCircle } from '~/components/user-circle'
+
+export function UserPanel({ users }: { users: User[] }) {
   return (
     <div className="flex w-1/6 flex-col bg-gray-200">
       <div className="flex h-20 items-center justify-center bg-gray-300 text-center">
         <h2 className="text-xl font-semibold text-blue-600">My team</h2>
       </div>
       <div className="flex flex-1 flex-col gap-y-10 overflow-y-scroll py-4">
-        <p>Users go here</p>
+        {users.map((user) => (
+          <UserCircle
+            key={user.id}
+            profile={user.profile}
+            className="mx-auto h-24 w-24 flex-shrink-0"
+          />
+        ))}
       </div>
       <div className="bg-gray-300 p-6 text-center">
         <button
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,25 @@
 import type { LoaderFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { json } from '@remix-run/node'
 import { requireUserId } from '~/utils/auth.server'
 import { Layout } from '~/components/layout'
 import { UserPanel } from '~/components/user-panel'
+import { getOtherUsers } from '~/utils/user.server'
 
 export const loader: LoaderFunction = async ({ request }) => {
-  await requireUserId(request)
-  return null // A loader has to return some value, even if that is null
+  const userId = await requireUserId(request)
+  const users = await getOtherUsers(userId)
+  console.log(users)
+  return json({ users }) // A loader has to return some value, even if that is null
 }
 
 export default function Home() {
+  const { users } = useLoaderData()
   return (
     <Layout>
       <div className="flex h-full">
-        <UserPanel />
+        <UserPanel users={users} />
+        <div className="flex-1"></div>
       </div>
     </Layout>
   )
